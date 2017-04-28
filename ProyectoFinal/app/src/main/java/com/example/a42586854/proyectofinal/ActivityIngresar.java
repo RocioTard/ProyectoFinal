713bbb9ea5a08cfef750af2399eb833f3e657517
ROCIO TARDÁ <42225260@ort>
2017-04-28 12:20:44 -0300

    proyecto entero aprox

---
+++ ProyectoFinal/app/src/main/java/com/example/a42586854/proyectofinal/ActivityIngresar.java
@@ -0,0 +1,15 @@
+package com.example.a42586854.proyectofinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class ActivityIngresar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingresar);
+    }
+}
